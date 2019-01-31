5b4491545e077f1175e9de81901cc44db34ff488
JustinAndrade <j.andrade.john@gmail.com>
2019-01-30 23:11:28 -0500

    main files updated

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const siteConttent = {
+const siteContent = {
   "nav": {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
@@ -40,3 +40,43 @@ const siteConttent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let ctaImage = document.getElementById('logo-img');
+logo.setAttribute('src', siteContent['nav']['img-src'])
+
+let middleImg = document.getElementById('middle-img');
+logo.setAttribute('src', siteContent['main-content']['middle-img-src'])
+
+let navAnchors = document.querySelectorAll('nav a');
+navAnchors[0].innerText = siteContent['nav']['nav-item-1'];
+navAnchors[1].innerText = siteContent['nav']['nav-item-2'];
+navAnchors[2].innerText = siteContent['nav']['nav-item-3'];
+navAnchors[3].innerText = siteContent['nav']['nav-item-4'];
+navAnchors[4].innerText = siteContent['nav']['nav-item-5'];
+navAnchors[5].innerText = siteContent['nav']['nav-item-6'];
+
+let ctaButton = document.querySelector('button')
+ctaButton.innerHTML = siteContent.cta.button;
+
+// let ctaButton = document.querySelectorAll()
+
+// let ctaHeader = document.querySelector(['h1']);
+// ctaHeader.innerText.cta.h1;
+
+let topContentHeaders = document.querySelectorAll('.top-content .text-content h4');
+topContentHeaders[0].innerText = siteContent["main-content"]["features-h4"];
+topContentHeaders[1].innerText = siteContent["main-content"]["about-h4"];
+
+let topContentText = document.querySelectorAll('.top-content .text-content p');
+topContentText[0].innerText = siteContent["main-content"]["features-content"];
+topContentText[1].innerText = siteContent["main-content"]["about-content"];
+
+let bottomContentHeaders = document.querySelectorAll('.bottom-content .text-content h4');
+bottomContentHeaders[0].innerText = siteContent["main-content"]["services-h4"];
+bottomContentHeaders[1].innerText = siteContent["main-content"]["product-h4"];
+bottomContentHeaders[2].innerText = siteContent["main-content"]["vision-h4"];
+
+let bottomContentText = document.querySelectorAll('.bottom-content .text-content p');
+bottomContentText[0].innerText = siteContent["main-content"]["services-content"];
+bottomContentText[1].innerText = siteContent["main-content"]["product-content"];
+bottomContentText[2].innerText = siteContent["main-content"]["vision-content"];
